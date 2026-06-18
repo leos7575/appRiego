57e27a155f8757d4a0aae0d587742959cd9e5157
Nohemi <159970612+NohemiMimi@users.noreply.github.com>
2026-06-18 13:20:13 -0600

    nueva API

---
+++ config/config.ts
@@ -0,0 +1,6 @@
+export const _URL_API='https://api-riego-i0yt0iifo-leonardos-projects-2f92a5fc.vercel.app/' //https://apiriego-srnq.onrender.com/
+export const _URL_USERS=_URL_API+"mensaje"
+export const _URL_USERS_IN=_URL_API+"insert_user"
+export const _URL_SECTOR1=_URL_API+"update_sector1/"
+export const _URL_SECTOR2=_URL_API+"update_sector2/"
+export const _URL_SECTOR1_ESTADO=_URL_API+"estado/"
